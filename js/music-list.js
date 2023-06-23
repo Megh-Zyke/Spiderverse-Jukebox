9d986f5e4611f192dcff6114d03556e39fef346e
Meghanand Gejjela <97515984+Megh-Zyke@users.noreply.github.com>
2023-06-23 11:17:16 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,52 @@
+let allMusic = [
+    {
+        name:"Am I Dreaming",
+        artist: "Metro Boomin, A$AP Rocky, Roisee",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Am I Dreaming (Visualizer).mp3"
+    },
+    {
+        name:"Annihilte",
+        artist: "Metro Boomin",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Annihilate.mp3"
+    },
+    {
+        name:"Calling",
+        artist: "Metro Boomin, NAV, A Boogie wit da Hoodie, Swae Lee",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Calling.mp3"
+    },
+
+    {
+        name:"Link Up",
+        artist: "Metro Boomin, Don Toliver ",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Link Up.mp3"
+    },
+    {
+        name:"Mona Lisa",
+        artist: "Dominic Fike",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Mona Lisa.mp3"
+    },
+    {
+        name:"Self Love ",
+        artist: "Metro Boomin, Coi Leray",
+        img : "Spider-Man-Across-The-Spider-Verse-4K-Wallpaper.jpg",
+        src : "songs/Self Love.mp3"
+    },
+    {
+        name:"Sunflower ",
+        artist: "Post Malone",
+        img : "800x600_Spider-man_onsale7.jpg",
+        src : "songs/Sunflower.mp3"
+    },
+    {
+        name:"What's Up Danger ",
+        artist: "Blackway & Black Caviar",
+        img : "800x600_Spider-man_onsale7.jpg",
+        src : "songs/What's Up Danger.mp3"
+    }
+
+]
